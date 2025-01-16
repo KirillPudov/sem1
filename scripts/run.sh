468d5524730541c66d5653e645bd09319f07c21e
Kirill Pudov <kereeell@Kirills-MacBook-Pro.local>
2025-01-17 01:04:34 +0300

    exec test

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -x
 
-exec env POSTGRES_HOST=localhost POSTGRES_PORT=5432 POSTGRES_DB=project-sem-1 POSTGRES_USER=validator POSTGRES_PASSWORD=val1dat0r project_sem
+POSTGRES_HOST=localhost POSTGRES_PORT=5432 POSTGRES_DB=project-sem-1 POSTGRES_USER=validator POSTGRES_PASSWORD=val1dat0r exec project_sem
