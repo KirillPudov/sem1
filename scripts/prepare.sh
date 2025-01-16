972c9da2f38d5224e5bcfb449db96deb640a5013
Kirill Pudov <kereeell@Kirills-MacBook-Pro.local>
2025-01-17 00:33:43 +0300

    add prepare and run scripts

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+set -e
+go mod tidy
+go build -buildvcs=false -o server
+
+export PGPASSWORD=val1dat0r
+
+psql -U validator -d project-sem-1 -h localhost -c 'CREATE TABLE prices("id" int primary key, "name" varchar(20), "category" varchar(40), "price" float, "create_date" date)'
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,3 @@
 #!/bin/bash
+set -e
+chmod +x server && ./server
