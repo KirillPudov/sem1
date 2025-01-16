3cc71fee0752e4f3605a7e54a37db04839b8e48e
Kirill Pudov <kereeell@Kirills-MacBook-Pro.local>
2025-01-17 01:10:51 +0300

    exec fg

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -2,6 +2,10 @@
 set -x
 go mod tidy
 
+go build -buildvcs=false -o server
+
+chmod +x server
+
 export PGPASSWORD=val1dat0r
 
 psql -U validator -d project-sem-1 -h localhost -c 'CREATE TABLE prices("id" int primary key, "name" varchar(20), "category" varchar(40), "price" float, "create_date" date)'
